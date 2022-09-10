02302761077800165a565debb5278ad0812b1320
Tanvir <tanvir01862@gmail.com>
2022-09-10 15:55:59 +0600

    end templatting

---
+++ resources/views/layouts/partial/js.blade.php
@@ -0,0 +1,27 @@
+    <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+
+    {{-- <script src="{{asset('assets')}}/js/jquery-3.6.0.min.js"></script> --}}
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+
+    {{-- <script src="{{asset('assets')}}/js/bootstrap.bundle.min.js"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+    </script>
+
+    <script src="{{ asset('assets') }}/js/jquery.slimscroll.min.js"></script>
+
+    <script src="{{ asset('assets') }}/plugins/morris/morris.min.js"></script>
+    <script src="{{ asset('assets') }}/plugins/raphael/raphael.min.js"></script>
+    <script src="{{ asset('assets') }}/js/chart.js"></script>
+
+
+    <script src="{{ asset('assets') }}/js/jquery.dataTables.min.js"></script>
+    <script src="{{ asset('assets') }}/js/dataTables.bootstrap4.min.js"></script>
+    <script src="{{ asset('assets') }}/js/moment.min.js"></script>
+    <script src="{{ asset('assets') }}/js/bootstrap-datetimepicker.min.js"></script>
+
+    <script src="{{ asset('assets') }}/js/select2.min.js"></script>
+    <script src="{{ asset('assets') }}/js/multiselect.min.js"></script>
+
+    <script src="{{ asset('assets') }}/js/app.js"></script>
---
+++ resources/views/layouts/partial/style.blade.php
@@ -0,0 +1,20 @@
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets') }}/img/favicon.png">
+
+<link rel="stylesheet" href="{{ asset('assets') }}/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="{{ asset('assets') }}/css/font-awesome.min.css">
+
+<link rel="stylesheet" href="{{ asset('assets') }}/css/line-awesome.min.css">
+
+<link rel="stylesheet" href="{{ asset('assets') }}/plugins/morris/morris.css">
+
+
+<link rel="stylesheet" href="{{ asset('assets') }}/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="{{ asset('assets') }}/css/bootstrap-datetimepicker.min.css">
+
+<link rel="stylesheet" href="{{ asset('assets') }}/css/select2.min.css">
+
+<link rel="stylesheet" href="{{ asset('assets') }}/plugins/bootstrap-tagsinput/bootstrap-tagsinput.css">
+
+
+<link rel="stylesheet" href="{{ asset('assets') }}/css/style.css">
